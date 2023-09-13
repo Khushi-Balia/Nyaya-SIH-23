813d494cab243a813a2fd3a50c600e0b3d0401cd
Adesh <sawantadesh2003@gmail.com>
2023-09-13 13:45:49 +0530

    added update password route

--- server/app.py
+++ server/app.py
@@ -23,7 +23,7 @@ def signup():
     existing_user = users.find_one({"email": email})
     if existing_user:
         response = make_response(
-            jsonify({"msg": "User already exists!Login Instead"}),
+            jsonify({"success":False,"msg": "User already exists!Login Instead"}),
             409,
         )
         return response
@@ -40,16 +40,15 @@ def signup():
 
         # print(token)
         response = make_response(
-            jsonify({"msg": "User registered successfully"}),
+            jsonify({"success":True,"msg": "User registered successfully"}),
             200,
         )
         response.set_cookie(
             "token", token, expires=datetime.utcnow() + timedelta(hours=24)
         )
-        response.headers.add("Access-Control-Allow-Origin", "*")
         return response
 
-    return jsonify({"msg": "Sign Up failed"}), 400
+    return jsonify({"success":False,"msg": "Sign Up failed"}), 400
 
 
 @app.route("/users/login", methods=["POST"])
@@ -59,7 +58,7 @@ def login():
 
     existing_user = users.find_one({"email": email})
     if not existing_user:
-        return jsonify({"msg": "User does not exist!Signup instead!"}), 400
+        return jsonify({"success":False,"msg": "User does not exist!Signup instead!"}), 400
 
     hashed_password = existing_user["password"]
     check_password = bcrypt.check_password_hash(hashed_password, password)
@@ -69,7 +68,7 @@ def login():
             app.config["SECRET_KEY"],
         )
         response = make_response(
-            jsonify({"msg": "Logged in successfully"}),
+            jsonify({"success":True,"msg": "Logged in successfully"}),
             200,
         )
         response.set_cookie(
@@ -80,15 +79,15 @@ def login():
 
     elif not check_password:
         return (
-            jsonify({"msg": "Incorrect email or password!"}),
+            jsonify({"success":False,"msg": "Incorrect email or password!"}),
             400,
         )
-    return jsonify({"msg": "Login failed"}), 400
+    return jsonify({"success":False,"msg": "Login failed"}), 400
 
 
 @app.route("/users/logout", methods=["POST"])
 def logout():
-    response = jsonify({"msg": "Logout successful"})
+    response = jsonify({"success":True,"msg": "Logout successful"})
     response.set_cookie("token", "", expires=0)
     return response
 
@@ -97,7 +96,7 @@ def logout():
 def profile():
     token_cookie = request.cookies.get("token")
     if not token_cookie:
-        return jsonify({"msg": "User not logged in"}), 401
+        return jsonify({"success":False,"msg": "User not logged in"}), 401
 
     try:
         decoded = jwt.decode(
@@ -106,19 +105,19 @@ def profile():
         email = decoded.get("email")
         existing_user = users.find_one({"email": email})
         user = {"name": existing_user["name"], "email": existing_user["email"]}
-        return jsonify({"user": user, "msg": "User authenticated successfully"}), 200
+        return jsonify({"success":True,"user": user, "msg": "User authenticated successfully"}), 200
     except jwt.ExpiredSignatureError:
-        return jsonify({"msg": "Token has expired"}), 401
+        return jsonify({"success":False,"msg": "Token has expired"}), 401
 
     except jwt.InvalidTokenError:
-        return jsonify({"msg": "Invalid token"}), 401
+        return jsonify({"success":False,"msg": "Invalid token"}), 401
     
 
 @app.route("/users/deleteprofile")
 def deleteprofile():
     token_cookie = request.cookies.get("token")
     if not token_cookie:
-        return jsonify({"msg": "User not logged in"}), 401
+        return jsonify({"success":False,"msg": "User not logged in"}), 401
 
     try:
         decoded = jwt.decode(
@@ -128,21 +127,21 @@ def deleteprofile():
         existing_user = users.find_one({"email": email})
         user = {"name": existing_user["name"], "email": existing_user["email"]}
         users.delete_one(existing_user)
-        response = jsonify({"user": user, "msg": "User deleted successfully"})
+        response = jsonify({"success":True,"user": user, "msg": "User deleted successfully"})
         response.set_cookie("token", "", expires=0)
         return response,200
     except jwt.ExpiredSignatureError:
-        return jsonify({"msg": "Token has expired"}), 401
+        return jsonify({"success":False,"msg": "Token has expired"}), 401
 
     except jwt.InvalidTokenError:
-        return jsonify({"msg": "Invalid token"}), 401
+        return jsonify({"success":False,"msg": "Invalid token"}), 401
     
 
 @app.route("/users/updateprofile",methods=["POST"])
 def updateprofile():
     token_cookie = request.cookies.get("token")
     if not token_cookie:
-        return jsonify({"msg": "User not logged in"}), 401
+        return jsonify({"success":False,"msg": "User not logged in"}), 401
 
     try:
         decoded = jwt.decode(
@@ -153,15 +152,44 @@ def updateprofile():
         users.update_one({"email": email},{ "$set": { "name": updated_name } })
         existing_user = users.find_one({"email": email})
         user = {"name": existing_user["name"], "email": existing_user["email"]}
-        response = jsonify({"user": user, "msg": "Username updated successfully"})
+        response = jsonify({"success":True,"user": user, "msg": "Username updated successfully"})
         return response,200
     except jwt.ExpiredSignatureError:
-        return jsonify({"msg": "Token has expired"}), 401
+        return jsonify({"success":False,"msg": "Token has expired"}), 401
 
     except jwt.InvalidTokenError:
-        return jsonify({"msg": "Invalid token"}), 401
+        return jsonify({"success":False,"msg": "Invalid token"}), 401
 
+@app.route("/users/updatepassword",methods=["POST"])
+def updatepassword():
+    token_cookie = request.cookies.get("token")
+    if not token_cookie:
+        return jsonify({"success":False,"msg": "User not logged in"}), 401
+    try:
+        oldpassword=request.json["oldpassword"]
+        newpassword=request.json["newpassword"]
+        confirmpassword=request.json["confirmpassword"]
+        decoded = jwt.decode(
+            token_cookie, app.config["SECRET_KEY"], algorithms=["HS256"]
+        )
+        email = decoded.get("email")
+        existing_user=users.find_one({"email":email})
+        hashed_password = existing_user["password"]
+        check_password = bcrypt.check_password_hash(hashed_password,oldpassword)
+        if check_password==0:
+            return jsonify({"success":False,"msg":"Please try again with the correct credentials"})   
+        if newpassword!=confirmpassword:
+            return jsonify({"success":False,"msg":"Please try again with the correct credentials"})    
+        myquery = { "email": email}
+        hashed_password = bcrypt.generate_password_hash(newpassword).decode("utf-8")
+        newvalues = { "$set": { "password": hashed_password } }
+        users.update_one(myquery, newvalues)
+        return jsonify({"success":True,"msg":"Password updated sucessfully"}) , 200
+    except jwt.ExpiredSignatureError:
+        return jsonify({"success":False,"msg": "Token has expired"}), 401
 
+    except jwt.InvalidTokenError:
+        return jsonify({"success":False,"msg": "Invalid token"}), 401
 
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
